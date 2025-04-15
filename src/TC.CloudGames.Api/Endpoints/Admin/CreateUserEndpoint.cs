16ae6254006187d1555c6f0027b95084c00219e2
Rodrigo Presser <rodrigo.presser@dreamdev.io>
2025-04-14 22:37:47 -0300

    password validation

--- src/TC.CloudGames.Api/Endpoints/Admin/CreateUserEndpoint.cs
+++ src/TC.CloudGames.Api/Endpoints/Admin/CreateUserEndpoint.cs
@@ -24,7 +24,7 @@ namespace TC.CloudGames.Api.Endpoints.Admin
         {
             RuleFor(x => x.Name)
                 .NotEmpty()
-                .WithMessage("Name is required.");
+                .WithMessage("First name is required.");
 
             RuleFor(x => x.Email)
                 .NotEmpty()
@@ -37,12 +37,24 @@ namespace TC.CloudGames.Api.Endpoints.Admin
                 .WithMessage("Role is required.")
                 .Must(role => role == "Admin" || role == "User")
                 .WithMessage("Role must be either 'Admin' or 'User'.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.")
+                .Matches(@"[A-Z]")
+                .WithMessage("Password must contain at least one uppercase letter.")
+                .Matches(@"[a-z]")
+                .WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"\d")
+                .WithMessage("Password must contain at least one number.")
+                .Matches(@"[\W_]")
+                .WithMessage("Password must contain at least one special character.");
         }
     }
 
-    public record RegisterRequest(string Name, string Email, string Role);
+    public record RegisterRequest(string Name, string Email, string Password, string Role);
 
     public record RegisterResponse(string Id, string Name, string Email, string Role);
-
-    public record UserDto(string Id, string Name, string Email, string Role);
 }
