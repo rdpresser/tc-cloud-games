d61aafd1902f8deb192b8d42935e2db745332b7c
Rodrigo Presser <rodrigo.presser@gmail.com>
2025-05-19 16:50:23 -0300

    Refactor SerilogExtensions for clarity and coverage
    Added [ExcludeFromCodeCoverage] attribute to SerilogExtensions class. Improved readability of UseCustomSerilog method by restructuring logger configuration. Updated Program.cs to utilize the new logging setup.

--- src/TC.CloudGames.Api/Extensions/SerilogExtensions.cs
+++ src/TC.CloudGames.Api/Extensions/SerilogExtensions.cs
@@ -4,36 +4,32 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace TC.CloudGames.Api.Extensions
 {
-    namespace TC.CloudGames.Api.Extensions
+    [ExcludeFromCodeCoverage]
+    public static class SerilogExtensions
     {
-        [ExcludeFromCodeCoverage]
-        public static class SerilogExtensions
+        public static IHostBuilder UseCustomSerilog(this IHostBuilder hostBuilder, IConfiguration configuration)
         {
-            public static IHostBuilder UseCustomSerilog(this IHostBuilder hostBuilder, IConfiguration configuration)
+            return hostBuilder.UseSerilog((hostContext, services, loggerConfiguration) =>
             {
-                return hostBuilder.UseSerilog((hostContext, services, loggerConfiguration) =>
-                {
-                    loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration);
+                loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration);
 
-                    var timeZoneId = configuration["TimeZone"] ?? "UTC"; // Default to UTC if not specified
-                    var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+                var timeZoneId = configuration["TimeZone"] ?? "UTC"; // Default to UTC if not specified
+                var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
 
-                    loggerConfiguration
-                        .WriteTo.Logger(lc => lc
-                            .MinimumLevel.Debug()
-                            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                            .Enrich.FromLogContext()
-                            .Enrich.WithProperty("Application", "TC.CloudGames.Api")
-                            .Enrich.WithProperty("Environment", hostContext.HostingEnvironment.EnvironmentName)
-                            .Enrich.With(new UtcToLocalTimeEnricher(timeZone)) // Pass the timezone to the enricher
-                            .WriteTo.Console(outputTemplate: "[{LocalTimestamp}] {Level:u3} {Message:lj}{NewLine}{Exception}"))
-                            .Enrich.WithSensitiveDataMasking(options =>
-                            {
-                                options.MaskProperties = ["Password", "Email", "PhoneNumber"];
-                            });
-                });
-            }
+                loggerConfiguration
+                    .WriteTo.Logger(lc => lc
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .Enrich.FromLogContext()
+                        .Enrich.WithProperty("Application", "TC.CloudGames.Api")
+                        .Enrich.WithProperty("Environment", hostContext.HostingEnvironment.EnvironmentName)
+                        .Enrich.With(new UtcToLocalTimeEnricher(timeZone)) // Pass the timezone to the enricher
+                        .WriteTo.Console(outputTemplate: "[{LocalTimestamp}] {Level:u3} {Message:lj}{NewLine}{Exception}"))
+                        .Enrich.WithSensitiveDataMasking(options =>
+                        {
+                            options.MaskProperties = ["Password", "Email", "PhoneNumber"];
+                        });
+            });
         }
     }
-
 }
--- src/TC.CloudGames.Api/Program.cs
+++ src/TC.CloudGames.Api/Program.cs
@@ -1,5 +1,3 @@
-using TC.CloudGames.Api.Extensions.TC.CloudGames.Api.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseCustomSerilog(builder.Configuration);
