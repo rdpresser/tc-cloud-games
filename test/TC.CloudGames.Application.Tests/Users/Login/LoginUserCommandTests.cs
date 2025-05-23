5884f14cece5fca453cfd688c1f323e740cdc4fd
Ricardo <ricardocavalcantesilva@gmail.com>
2025-05-22 22:25:58 -0300

    Create LoginUserCommandTests.cs

---
+++ test/TC.CloudGames.Application.Tests/Users/Login/LoginUserCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using TC.CloudGames.Application.Users.Login;
+
+namespace TC.CloudGames.Application.Tests.Users.Login;
+
+public class LoginUserCommandTests
+{
+    [Fact]
+    public async Task LoginUserCommand_ShouldReturnSuccess_WhenValidCredentials()
+    {
+        // Arrange
+        var email = "teste@exemplo.com";
+        var senha = "senha123";
+        
+        // Act
+        var command = new LoginUserCommand(email, senha);
+        
+        //Assert
+        Assert.Equal(email, command.Email);
+        Assert.Equal(senha, command.Password);
+    }
+}
