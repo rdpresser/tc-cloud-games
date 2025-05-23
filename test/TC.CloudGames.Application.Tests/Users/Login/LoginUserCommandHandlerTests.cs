6aefeef61b94ddfc6e2295dcd57c46c19e65c163
Ricardo <ricardocavalcantesilva@gmail.com>
2025-05-22 22:40:18 -0300

    Create LoginUserCommandHandlerTests

---
+++ test/TC.CloudGames.Application.Tests/Users/Login/LoginUserCommandHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.Result;
+using FakeItEasy;
+using FastEndpoints;
+using TC.CloudGames.Application.Abstractions.Messaging;
+using TC.CloudGames.Application.Users.Login;
+using TC.CloudGames.Domain.User.Abstractions;
+
+namespace TC.CloudGames.Application.Tests.Users.Login;
+
+public class LoginUserCommandHandlerTests
+{
+    [Fact]
+    public async Task LoginUserCommandHandler_LoginUser_Ok()
+    {
+        // Arrange
+        Factory.RegisterTestServices(_ => {});
+        
+        var loginReq = new LoginUserCommand(Email: "teste@exemplo.com", Password: "senha123");
+        var loginRes = new LoginUserResponse("<jwt-token>", "teste@exemplo.com");
+        
+        var fakeHandler = A.Fake<CommandHandler<LoginUserCommand, LoginUserResponse, Domain.User.User, IUserEfRepository>>();
+        A.CallTo(() => fakeHandler.ExecuteAsync(A<LoginUserCommand>.Ignored, A<CancellationToken>.Ignored))
+            .Returns(Task.FromResult(Result<LoginUserResponse>.Success(loginRes)));
+        
+        // Act
+        var result = await fakeHandler.ExecuteAsync(loginReq, CancellationToken.None);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal("<jwt-token>", result.Value.JwtToken);
+    }
+}
