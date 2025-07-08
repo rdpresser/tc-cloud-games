e5f7fbe98e843e8590d125e668b68e411c5ea9dc
rdpresser <rodrigo.presser@gmail.com>
2025-07-08 17:04:29 -0300

    Add Grafana Loki logging support
    Updated `SerilogExtensions.cs` to include a new configuration parameter `GrafanaLokiUrl` and added a sink for Grafana Loki to the logger configuration. Ensured that the `GrafanaLokiUrl` is validated for null or empty values.
    
    Modified `appsettings.Development.json` to include the new `GrafanaLokiUrl` entry and updated the Serilog configuration to support the Grafana Loki sink while retaining existing sinks for Console and Seq.

--- src/TC.CloudGames.Api/Extensions/SerilogExtensions.cs
+++ src/TC.CloudGames.Api/Extensions/SerilogExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Serilog.Enrichers.Sensitive;
 using Serilog.Events;
+using Serilog.Sinks.Grafana.Loki;
 using System.Diagnostics.CodeAnalysis;
 
 namespace TC.CloudGames.Api.Extensions
@@ -16,6 +17,12 @@ namespace TC.CloudGames.Api.Extensions
                 var timeZoneId = configuration["TimeZone"] ?? "UTC"; // Default to UTC if not specified
                 var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
 
+                var grafanaLokiUrl = configuration["GrafanaLokiUrl"];
+                if (string.IsNullOrEmpty(grafanaLokiUrl))
+                {
+                    throw new ArgumentNullException(nameof(grafanaLokiUrl), "GrafanaLokiUrl configuration is required.");
+                }
+
                 loggerConfiguration
                     .WriteTo.Logger(lc => lc
                         .MinimumLevel.Debug()
@@ -23,12 +30,22 @@ namespace TC.CloudGames.Api.Extensions
                         .Enrich.FromLogContext()
                         .Enrich.WithProperty("Application", "TC.CloudGames.Api")
                         .Enrich.WithProperty("Environment", hostContext.HostingEnvironment.EnvironmentName)
-                        .Enrich.With(new UtcToLocalTimeEnricher(timeZone)) // Pass the timezone to the enricher
-                        .WriteTo.Console(outputTemplate: "[{LocalTimestamp}] {Level:u3} {Message:lj}{NewLine}{Exception}"))
-                        .Enrich.WithSensitiveDataMasking(options =>
-                        {
-                            options.MaskProperties = ["Password", "Email", "PhoneNumber"];
-                        });
+                        .Enrich.With(new UtcToLocalTimeEnricher(timeZone))
+                        .WriteTo.Console(outputTemplate: "[{LocalTimestamp}] {Level:u3} {Message:lj}{NewLine}{Exception}")
+                        .WriteTo.GrafanaLoki(
+                            uri: grafanaLokiUrl,
+                            credentials: new LokiCredentials { Login = "1267846", Password = Environment.GetEnvironmentVariable("GRAFANA_API_TOKEN") },
+                            labels:
+                            [
+                                new LokiLabel{ Key = "app", Value= "tccloudgames-app" },
+                                new LokiLabel{ Key = "env", Value = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development" }
+                            ]
+                        )
+                    )
+                    .Enrich.WithSensitiveDataMasking(options =>
+                    {
+                        options.MaskProperties = ["Password", "Email", "PhoneNumber"];
+                    });
             });
         }
     }
