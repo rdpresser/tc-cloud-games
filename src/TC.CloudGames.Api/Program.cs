719df54e6fa1371ef1ebb93ea8b8038bc3bb545e
rdpresser <rodrigo.presser@gmail.com>
2025-07-09 12:22:17 -0300

    Refactor environment variable loading
    Comment out the `GetEnvRoot` method and simplify the
    loading of environment variables by directly referencing
    the `.env` file in the current directory. This change
    removes the conditional logic based on the operating
    system, streamlining the process.

--- src/TC.CloudGames.Api/Program.cs
+++ src/TC.CloudGames.Api/Program.cs
@@ -1,26 +1,23 @@
-using System.Runtime.InteropServices;
-using TC.CloudGames.Infra.CrossCutting.Commons.Extensions;
 using ServiceCollectionExtensions = TC.CloudGames.Api.Extensions.ServiceCollectionExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string GetEnvRoot()
-{
-    // If running in Docker, likely Linux and /app or /src will exist
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-    {
-        // On Windows, start from current directory
-        return SolutionRootFinder.FindRoot();
-    }
-    else
-    {
-        // On Linux (Docker), try /app and /src
-        return SolutionRootFinder.FindRoot(".solution-root", "/TC.CloudGames.Unit.Tests", "/app", "/src");
-    }
-}
+//string GetEnvRoot()
+//{
+//    // If running in Docker, likely Linux and /app or /src will exist
+//    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+//    {
+//        // On Windows, start from current directory
+//        return SolutionRootFinder.FindRoot();
+//    }
+//    else
+//    {
+//        // On Linux (Docker), try /app and /src
+//        return SolutionRootFinder.FindRoot(".solution-root", "/TC.CloudGames.Unit.Tests", "/app", "/src");
+//    }
+//}
 
-var root = GetEnvRoot();
-DotNetEnv.Env.Load(Path.Combine(root, ".env"));
+DotNetEnv.Env.Load(Path.Combine("./", ".env"));
 
 // TODO: Make this conditional based on environment
 builder.Host.UseCustomSerilog(builder.Configuration);
