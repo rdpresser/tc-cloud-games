6e242c6a64b8178051b9bff87f19a2dd7274b15f
rdpresser <rodrigo.presser@gmail.com>
2025-07-15 23:10:51 -0300

    Fix formatting inconsistencies in variable default values in variables.tf

--- infra/terraform/dev/variables.tf
+++ infra/terraform/dev/variables.tf
@@ -17,15 +17,15 @@ variable "postgres_admin_password" {
 }
 variable "app_object_id" {
   type    = string
-  default = "d240991c-b9f9-446e-b890-0ff307e34ab4" 
+  default = "d240991c-b9f9-446e-b890-0ff307e34ab4"
 }
 
 variable "app_object_id_github_actions" {
   type    = string
-  default = "311e0933-e635-4e8f-af82-a6e2bf200318" 
+  default = "311e0933-e635-4e8f-af82-a6e2bf200318"
 }
 
 variable "user_object_id" {
   type    = string
-  default = "887b7d92-985f-499b-b26d-9cc0785358cc" 
+  default = "887b7d92-985f-499b-b26d-9cc0785358cc"
 }
