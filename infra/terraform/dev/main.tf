cdc4ff92aa57eb028a34d34d72400053690e4e15
rdpresser <rodrigo.presser@gmail.com>
2025-07-20 22:54:51 -0300

    Update Azure Container Registry SKU from Basic to Standard for enhanced capabilities

--- infra/terraform/dev/main.tf
+++ infra/terraform/dev/main.tf
@@ -407,7 +407,7 @@ resource "azurerm_container_registry" "acr" {
   name                = local.acr_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Basic"
+  sku                 = "Standard"
   admin_enabled       = true
 
   tags = local.common_tags
